564df84e1a1de82270e5a59045064cf87709c83f
gabriellm1 <gabriellopesmonteiro41@gmail.com>
2020-04-30 01:22:26 -0300

    codigo r7 pronto

--- exemplo.php
+++ exemplo.php
@@ -1,38 +1,55 @@
-{
-$n = readline();
-$i = 1;
-$x = 1;
-$y = 4;
-  while (($i < $n) or ($i == $n)) {
-    if ($x > $y)
-      $y = $y + 1;
-    else if ($x < $y) 
-      $x = $x + 1;
-    else {
-            $x = $x + 1;
-        }   
-        $i = $i + 1;
+<?php
+  {
+  $n = readline();
+  $i = 1;
+  $x = 1;
+  $y = 4;
+    $init  = "começou primeiro loop";
+    while (($i < $n) or ($i == $n)) {
+      if ($x > $y){
+        $y = $y + 1;
+      }
+      else if ($x < $y) 
+        $x = $x + 1;
+      else {
+              $x = $x + 1;
+          }   
+          $i = $i + 1;
+      }
+    $init = $init . " mas ja acabou";
+    echo $init;
+    $g = readline(); /* coloque 0 */
+    $t = TrUe;
+    $counter = 0;
+    while (!$g and $t) {
+        if ($g or $t)
+          $counter = $counter + 1;
+          
+        if ($counter == 5){
+          echo "acabou segundo loop";
+          $g = TRue; 
+          $t = faLse;  
+        } 
     }
-
-  $g = 0;
-  $t = 1;
-  $counter = 0;
-  while (!$g and $t) {
-      if ($g or $t)
-        $counter = $counter + 1;
-        
-      if ($counter == 5){
-        $g = 1; 
-        $t = 0;  
-      } 
-  }
-echo $counter;
-echo $g;
-echo $t;
-  if ($g and !$t){
+  echo $counter;
+  echo $g;
+  echo $t;
+    if ($g and !$t){
+      echo $x;
+      echo $y;
+      echo $i;
+    }
+    if ("A"=="A") echo readline();
+    if ("B" == "A") echo "devia dar erro";
+    
+    $x = 1 + True; /* Ok */
     echo $x;
-    echo $y;
-    echo $i;
+    $x = 1 and True; /* Ok */
+    echo $x;
+    $x = "a" . 1 . True; /* Ok */
+    echo $x;
+    $x = "a" == "b"; /* Ok, resultado bool: False */
+    echo $x;
+    if (tRuE==1) echo "fim do exemplo";
   }
-  echo readline();
-}
+?>
---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+/* arquivo para erros*/
+{
+        
+    /*$x = 1 + "a";*/
+    /*$x = True and "a";*/
+    /*$x = 5 < "a";*/
+    /*if (tRuE=="a") echo "deu ruim";*/
+    /*if (tRuE>"a") echo "deu um pouco ruim";*/
+    /*while ("A"+1) echo "deu bem ruim";*/
+}    
+?>
